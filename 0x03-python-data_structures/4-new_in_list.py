daa3b1cc6df71326b6f5576dfc5f7c032168dbb2
oluwatobiloba <oluwateezzy03@gmail.com>
2023-02-01 12:30:19 -0800

    print a list in reverse

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     lis = my_list
-    if idx < 0 || idx > len(my_list):
+    if idx < 0 | idx > len(my_list):
         return lis
     for i in range(len(my_list)):
         if i == idx:
