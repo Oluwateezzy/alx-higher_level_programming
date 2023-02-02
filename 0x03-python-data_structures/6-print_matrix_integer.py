0127a74f569168edc09dd2c6e3ac74c35df5451f
oluwatobiloba <oluwateezzy03@gmail.com>
2023-02-02 06:12:15 -0800

    print a matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix is None:
+        print()
+    else:
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if j == len(matrix[i]) - 1:
+                    print("{}".format(j))
+                else:
+                    print("{}".format(j), end=" ")
