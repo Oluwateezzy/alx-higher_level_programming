bf22034a807fec9d73b1d6ce24ef31cca88f23b7
oluwatobiloba <oluwateezzy03@gmail.com>
2023-02-02 05:26:20 -0800

    replace a copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     lis = my_list
+    copy = []
     if idx < 0 | idx > len(my_list):
         return lis
     for i in range(len(my_list)):
         if i == idx:
-            lis[i] = element
-    return lis
+            copy.append(element)
+        else:
+            copy.append(my_list[i])
+    return copy
