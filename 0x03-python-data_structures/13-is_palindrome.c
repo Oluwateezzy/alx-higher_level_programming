67a9089caede51b3be76358a4e464f7c17e84f25
oluwatobiloba <oluwateezzy03@gmail.com>
2023-02-03 03:33:02 -0800

    list palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * is_palindrome - check palindrome
+ * @head: linked list
+ * return: int
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *temp;
+	int count, i, j;
+	int *list;
+
+	temp = *head;
+	count = 0;
+	while(*head != NULL)
+	{
+		count = count + 1;
+		*head = (*head)->next;
+	}
+	list = malloc(sizeof(int *) * (count + 1));
+	if (list == NULL)
+		return (0);
+	i = 0;
+	while(temp != NULL)
+	{
+		list[i] = (temp)->n;
+		temp = (temp)->next;
+		i++;
+	}
+	for (i = 0, j = count - 1; i <= count - 1; i++, j--)
+	{
+		if(list[i] != list[j])
+		{
+			free(list);
+			return (0);
+		}
+	}
+	free(list);
+	return (1);
+}
