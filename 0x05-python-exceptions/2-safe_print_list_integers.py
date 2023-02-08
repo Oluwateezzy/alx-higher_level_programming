8326a69f1723e841680a3184cee30a94313b574f
oluwatobiloba <oluwateezzy03@gmail.com>
2023-02-08 05:27:48 -0800

    safe printing integer

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,6 +2,8 @@
 def safe_print_list_integers(my_list=[], x=0):
     try:
         for i in range(x):
+            if type(my_list[i]) not int:
+                pass
             print("{:d}".format(my_list[i]), end="")
     except ValueError:
         print()
