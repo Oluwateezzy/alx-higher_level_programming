4cf924227794ffe600abaf30f9ec444a4ad14358
oluwatobiloba <oluwateezzy03@gmail.com>
2023-02-09 11:22:16 -0800

    access and update attribute

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -4,17 +4,24 @@
 
 class Square:
     """Class Square"""
-    def size(self, value):
+    def __init__(self, size=0):
         """Constructor"""
-        if type(value) is not int:
+        if type(size) is not int:
             raise (TypeError("size must be an integer"))
-        elif value < 0:
+        elif size < 0:
             raise (ValueError("size must be >= 0"))
         else:
-            self.__size = value
+            self.__size = size
 
     def area(self):
         """area of square"""
         return (self.__size ** 2)
     def size(self):
         return self.__size
+    def size(self, value):
+        if type(value) is not int:
+            raise (TypeError("size must be an integer"))
+        elif value < 0:
+            raise (ValueError("size must be >= 0"))
+        else:
+            self.__size = value
