54e5f88572e8bb39b1161a4f827aec82403bd897
oluwatobiloba <oluwateezzy03@gmail.com>
2023-02-09 08:19:31 -0800

    square first

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Class Square with size"""
+
+
+class Square:
+    """Class Square with size"""
+
+    def __init__(self, size):
+        """Constructor"""
+        self.__size = size
