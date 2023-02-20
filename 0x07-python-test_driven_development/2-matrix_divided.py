7b92a61bb8f0c7b8ee7d481597ae4e3c5497a6cb
oluwatobiloba <oluwateezzy03@gmail.com>
2023-02-20 07:04:47 -0800

    matrix refactor

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -4,19 +4,15 @@
 
 
 def matrix_divided(matrix, div):
-     """Divide all elements of a matrix.
-    Args:
-        matrix (list): A list of lists of ints or floats.
-        div (int/float): The divisor.
-    Raises:
-        TypeError: If the matrix contains non-numbers.
-        TypeError: If the matrix contains rows of different sizes.
-        TypeError: If div is not an int or float.
-        ZeroDivisionError: If div is 0.
-    Returns:
-        A new matrix representing the result of the division.
     """
-
+    Args:
+    matrix
+    div
+    Raises:
+    TypeError
+    Return:
+    a new list
+    """
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             if type(matrix[i][j]) not in [int, float]:
