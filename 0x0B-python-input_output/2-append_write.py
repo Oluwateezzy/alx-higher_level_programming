2dfd6cef80de319930e3b71a9a21eed2a57bc4df
oluwatobiloba <oluwateezzy03@gmail.com>
2023-02-21 06:58:57 -0800

    append file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""append to a file"""
+
+
+def append_write(filename="", text=""):
+    """append to a file"""
+
+    with open(filename, mode='a', encoding="utf-8") as file:
+        file.write(text)
+        return (text)
