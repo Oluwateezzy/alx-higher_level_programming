342c0eb005408babb08ee878a883a45e7d2d1508
oluwatobiloba <oluwateezzy03@gmail.com>
2023-02-21 02:29:53 -0800

    text indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -14,6 +14,19 @@ def text_indentation(text):
             str1 = ""
         else:
             str1 += i
-    for i in strD:
-        print(i.strip())
-        print()
+    strD.append(str1)
+    for i in range(len(strD)):
+        if i == len(strD) - 1:
+            print(strD[i].strip())
+        else:
+            print(strD[i].strip())
+            print()
+text_indentation("""Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere? \
+Non autem hoc: igitur ne illud quidem. Fortasse id optimum, sed ubi illud: \
+Plus semper voluptatis? Teneo, inquit, finem illi videri nihil dolere. \
+Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum \
+rationi oboediens. Si id dicis, vicimus. Inde sermone vario sex illa a Dipylo \
+stadia confecimus. Sin aliud quid voles, postea. Quae animi affectio suum \
+cuique tribuens atque hanc, quam dico. Utinam quidem dicerent alium alio \
+beatiorem! Iam ruinas videres""")
