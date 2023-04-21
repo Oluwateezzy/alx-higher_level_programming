51bb433ecd2185c6302e1243da7c1b606a1ebf9b
oluwatobiloba <oluwateezzy03@gmail.com>
2023-04-20 18:03:01 -0700

    second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,4 +4,8 @@ if (!argv[2] | !argv[3]) {
   console.log(0);
   return;
 }
-console.log(argv.slice(2).sort().reverse()[1]);
+new_arr = [];
+for (let i =  0; i < argv.slice(2).length; i++) {
+  new_arr.push(Number(argv.slice(2)[i]));
+}
+console.log(new_arr.sort(function(a, b){return a - b}).reverse()[1]);
