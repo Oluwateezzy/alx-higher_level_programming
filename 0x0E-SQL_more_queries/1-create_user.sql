b4268fb821f8710ab2e9356d1c9cca8f7a0c27ca
oluwatobiloba <oluwateezzy03@gmail.com>
2023-05-08 10:03:35 -0700

    user privilege

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,6 @@
 --script that creates the MySQL server use
 --script that creates the MySQL server use
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
