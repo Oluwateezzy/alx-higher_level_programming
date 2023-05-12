1ea4788c9eb3fdbb601969529161d0004640238a
oluwatobiloba <oluwateezzy03@gmail.com>
2023-05-12 08:06:34 -0700

    oops

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -3,7 +3,3 @@
 const nbOccurences = (list, searchElement) => list.filter((ls) => searchElement == ls).length;
 
 module.exports = nbOccurences;
-
-console.log(nbOccurences([1, 2, 3, 4, 5, 6], 3));
-console.log(nbOccurences([3, 2, 3, 4, 5, 3, 3], 3));
-console.log(nbOccurences(["S", 12, "c", "S", "School", 8], "S"));
