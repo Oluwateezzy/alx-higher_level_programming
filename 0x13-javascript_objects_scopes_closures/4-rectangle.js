4e6da03d8daf897b06bdfe785ae8b40f6d934145
oluwatobiloba <oluwateezzy03@gmail.com>
2023-05-12 06:17:38 -0700

    rect

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -12,14 +12,14 @@ class Rectangle {
       console.log('X'.repeat(this.width));
     }
   }
-  
-  rotate() {
-    let temp = this.width;
+
+  rotate () {
+    const temp = this.width;
     this.width = this.height;
     this.height = temp;
   }
 
-  double() {
+  double () {
     this.width = 2 * this.width;
     this.height = 2 * this.height;
   }
