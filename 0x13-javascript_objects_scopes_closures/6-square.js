8be08983c216af32c735d9f6fd1007826a4ec9e0
oluwatobiloba <oluwateezzy03@gmail.com>
2023-05-12 07:04:15 -0700

    square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -5,13 +5,13 @@ const Squaree = require('./5-square');
 class Square extends Squaree {
   charPrint (c) {
     if (!c) {
-      c = "X";
+      c = 'X';
     }
     for (let i = 0; i < this.height; i++) {
       let str = '';
-        for (let j = 0; j < this.width; j++) {
-          str = str + c;
-	}
+      for (let j = 0; j < this.width; j++) {
+        str = str + c;
+      }
       console.log(str);
     }
   }
