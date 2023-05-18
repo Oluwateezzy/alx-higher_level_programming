87c5a68fafb61a8879544934b2c862ecba17940d
oluwatobiloba <oluwateezzy03@gmail.com>
2023-05-18 04:52:03 -0700

    node

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const fs = require('fs');
+const { argv } = require('process');
+const file1 = argv[2];
+const file2 = argv[3];
+const d_file = argv[4];
+
+const content1 = fs.readFileSync(file1, 'utf8');
+const content2 = fs.readFileSync(file2, 'utf8');
+
+const concat = content1 + content2;
+console.log(concat);
+fs.writeFileSync(d_file, concat, 'utf8');
