94ad488ec7e1c0e10a6565458a7fe65e016c0705
oluwatobiloba <oluwateezzy03@gmail.com>
2023-05-18 06:11:31 -0700

    lista all

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,22 +5,26 @@ import MySQLdb
 import sys
 
 
-username = sys.argv[1]
-password = sys.argv[2]
-database = sys.argv[3]
+def listall():
+    '''list all'''
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-connect = MySQLdb.connect(
+    connect = MySQLdb.connect(
         host = 'localhost',
         port = 3306,
         user = username,
         passwd = password,
         db = database
-)
-cursor = connect.cursor()
-cursor.execute("SELECT * FROM states ORDER BY id ASC;")
-rows = cursor.fetchall()
-cursor.close()
-connect.close()
+    )
+    cursor = connect.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC;")
+    rows = cursor.fetchall()
+    cursor.close()
+    connect.close()
+    for row in rows:
+        print(row)
 
-for row in rows:
-    print(row)
+if __name__ == '__main__':
+    list_all()
