6b044f3e0fc8c825c064d7241dd837f6313d7cfc
oluwatobiloba <oluwateezzy03@gmail.com>
2023-05-22 11:46:00 -0700

    deletes

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+'''
+    Lists all state objs.
+'''
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    sql = sys.argv[2]
+    db = sys.argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(user, sql, db))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).order_by(State.id).all()
+    for state in states:
+        if 'a' in state.name:
+            session.delete(state)
+    session.commit()
+
+    session.close()
+    engine.dispose()
