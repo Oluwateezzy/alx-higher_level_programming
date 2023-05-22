73310ea0de83aa0e02dad7cf7672df43612d0a68
oluwatobiloba <oluwateezzy03@gmail.com>
2023-05-22 06:10:31 -0700

    fix

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -14,13 +14,13 @@ def listall():
     connect = MySQLdb.connect(host='localhost', port=3306,
                               user=user, passwd=pas, db=db)
     cursor = connect.cursor()
-    cursor.execute('SELECT * FROM states WHERE name regexp "^N.*" ' +
-                    'ORDER BY states.id ASC')
+    cursor.execute('SELECT * FROM states ORDER BY id ASC;')
     rows = cursor.fetchall()
     cursor.close()
     connect.close()
     for row in rows:
-        print(row)
+        if row[1][0] == "N":
+            print(row)
 
 
 if __name__ == '__main__':
