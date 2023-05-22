ad71ab8d97be6595058b24b428f347e8da8f05bc
oluwatobiloba <oluwateezzy03@gmail.com>
2023-05-22 07:42:47 -0700

    matched

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -15,7 +15,7 @@ def listall():
     connect = MySQLdb.connect(host='localhost', port=3306,
                               user=user, passwd=pas, db=db)
     cursor = connect.cursor()
-    cursor.execute('SELECT * FROM states WHERE name LIKE "{:s}" ORDER BY id ASC;'.format(state))
+    cursor.execute('SELECT * FROM states WHERE BINARY name = "{}" ORDER BY id ASC;'.format(state))
     rows = cursor.fetchall()
     cursor.close()
     connect.close()
