59fe8c4fcda00a5d39e73cb091fe346fc85054fe
oluwatobiloba <oluwateezzy03@gmail.com>
2023-05-22 06:25:14 -0700

    matched

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''filter all state in database'''
+
+import MySQLdb
+import sys
+
+
+def listall():
+    '''list all'''
+    user = sys.argv[1]
+    pas = sys.argv[2]
+    db = sys.argv[3]
+    state = sys.argv[4]
+
+    connect = MySQLdb.connect(host='localhost', port=3306,
+                              user=user, passwd=pas, db=db)
+    cursor = connect.cursor()
+    cursor.execute('SELECT * FROM states WHERE name = "{}" ORDER BY id ASC;'.format(state))
+    rows = cursor.fetchall()
+    cursor.close()
+    connect.close()
+    for row in rows:
+        print(row)
+
+
+if __name__ == '__main__':
+    listall()
