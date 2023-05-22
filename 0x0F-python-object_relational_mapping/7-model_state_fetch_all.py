6bf6e3623e20422ab8eb49d8e5a48b6527b4617f
oluwatobiloba <oluwateezzy03@gmail.com>
2023-05-22 10:07:13 -0700

    god

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,22 +9,18 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-# Retrieve MySQL username, password, and database name from command line arguments
 user = sys.argv[1]
 sql = sys.argv[2]
 db = sys.argv[3]
 
-# Create engine and session
 engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                        .format(user, sql, db))
 Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# Query all State objects and sort them by states.id in ascending order
 states = session.query(State).order_by(State.id).all()
 
-# Display the results
 for state in states:
     print(f"{state.id}: {state.name}")
 
