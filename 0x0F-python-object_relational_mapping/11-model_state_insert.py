e24a99e0bcb3a67360aebd2cfde05389d2e34ac2
oluwatobiloba <oluwateezzy03@gmail.com>
2023-05-22 11:24:40 -0700

    state

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+'''
+    Lists all state objs.
+'''
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    sql = sys.argv[2]
+    db = sys.argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(user, sql, db))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    new = State(name = 'Louisiana')
+    session.add(new)
+    session.commit()
+
+    print(new.id)
+    session.close()
+    engine.dispose()
